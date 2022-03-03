b82da6d1e74dbb96558a3cb32f49d8e471190c97
Thomas <79402018+ellilglor@users.noreply.github.com>
2022-03-03 16:25:13 +0000

    added custom presence

--- src/events/discord/ready.js
+++ src/events/discord/ready.js
@@ -24,6 +24,7 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setPresence({ activities: [{ name: '/help', type: 'LISTENING' }], status: 'online' });
 
     //await setPermissions(client);
 	},
