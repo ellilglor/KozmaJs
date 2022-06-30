1cfd8a269fcb76429d91ab0fcefd8a5be3458a8f
Thomas <79402018+ellilglor@users.noreply.github.com>
2022-06-30 09:44:33 +0000

    updated /help

--- src/commands/information/help.js
+++ src/commands/information/help.js
@@ -9,9 +9,10 @@ module.exports = {
     const reply = buildEmbed()
       .setTitle('Here are all my commands:')
       .setDescription(
+        `**/bookchance** Get the % chance you have of getting at least 1 Book of Dark Rituals.\n\n` + 
         `**/clear** Deletes all the messages the bot has sent you.\n\n` + 
-        `**/convert** Convert your currency.\n\n` +
-        `**/findlogs** Makes the bot search the database for your item.\nA date can be added to make the bot stop looking.\nSpecial searches:\n- color only ~ example: autumn\n\n` +
+        `**/convert** Convert your currency. (glorified calculator)\n\n` +
+        `**/findlogs** Makes the bot search the database for your item.\n\n` +
         `**/lockbox** Gives information about a lockbox or tells you what box drops your item.\n\n` +
         `**/rate** Tells you the crowns per energy rate currently in use.\n\n` +
         `**/unbox** Simulate opening a box and be disappointed for free.\n\n` +
