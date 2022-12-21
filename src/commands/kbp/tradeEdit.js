9e0abbf60ad60723376b83450116177a04702c6a
Thomas <79402018+ellilglor@users.noreply.github.com>
2022-12-21 10:48:25 +0000

    updated latest command description + removed unused command

--- src/commands/kbp/tradeEdit.js
+++ src/commands/kbp/tradeEdit.js
@@ -8,7 +8,9 @@ module.exports = {
 		.setName('tradepostedit')
 		.setDescription(`Gives you 2 minutes to edit your tradeposts.`),
 	async execute(interaction) {
-    const reply = buildEmbed(interaction).setTitle('You have 2 minutes to edit your tradeposts.');
+    const reply = buildEmbed(interaction)
+      .setTitle('You have 2 minutes to edit your tradeposts.')
+      .setDescription('Using this command to bypass the slowmode will result in a timeout.');
     const role = interaction.guild.roles.cache.get(globals.editRoleId);
 
     await interaction.reply({ embeds: [reply], ephemeral: true });
@@ -16,6 +18,6 @@ module.exports = {
     await logCommand(interaction);
     await wait(120000);
     await interaction.member.roles.remove(role);
-	  await interaction.editReply({ embeds: [reply.setTitle('Your time is up!')] });
+	  await interaction.editReply({ embeds: [reply.setTitle('Your time is up!').setDescription(null)] });
   }  
 };
