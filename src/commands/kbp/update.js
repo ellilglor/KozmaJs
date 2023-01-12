f124487638432774e078973e6e9e33f7c4b27548
Thomas <79402018+ellilglor@users.noreply.github.com>
2023-01-12 19:03:10 +0000

    added timing to /update

--- src/commands/kbp/update.js
+++ src/commands/kbp/update.js
@@ -10,27 +10,37 @@ module.exports = {
 		.setDescription(`Kozma's Backpack staff only.`)
     .setDefaultMemberPermissions(perms.KickMembers | perms.BanMembers),
 	async execute(interaction) {
+    const startTime = performance.now();
     const reply = buildEmbed(interaction).setTitle('Executing /update');
     const stats = [], collectAll = true;
     const logChannel = interaction.client.channels.cache.get(globals.botLogsChannelId);
     const channel = interaction.client.channels.cache.get(globals.marketChannelId);
     const message = await channel.messages.fetch({ limit: 1 });
     const msg = message.first();
+    let totalTime = 0;
 
     await interaction.reply({ embeds: [reply], ephemeral: true });
 
     await updateRate(msg, logChannel);
-    await interaction.editReply({ embeds: [reply.setTitle('Finished reading market image')] });
+    
+    const marketTime = ((performance.now() - startTime)/1000).toFixed(2);
+    totalTime += parseFloat(marketTime);
+    await interaction.editReply({ embeds: [reply.setTitle(`Finished reading market image in ${marketTime} seconds`)] });
 
     for (const [name, id] of channels) {
+      const start = performance.now();
       const chnl = interaction.guild.channels.cache.get(id);
+      
       stats.push(await convertLogs(chnl, name, collectAll));
-      await interaction.editReply({ embeds: [reply.setTitle(`Finished ${name}`)] });
+      
+      const time = ((performance.now() - start)/1000).toFixed(2);
+      totalTime += parseFloat(time);
+
+      await interaction.editReply({ embeds: [reply.setTitle(`Finished ${name} in ${time} seconds`)] });
     }
     
     saveStats(stats);
-
     console.table(stats);
-    await interaction.editReply({ embeds: [reply.setTitle('Update completed!')] });
+    await interaction.editReply({ embeds: [reply.setTitle(`Update completed in ${(totalTime/60).toFixed(2)} minutes!`)] });
 	}
 };
